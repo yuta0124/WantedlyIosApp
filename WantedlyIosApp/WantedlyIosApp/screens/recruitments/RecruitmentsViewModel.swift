6620929d027beda1b8273c779d0b45ec682e2d6e
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-07-23 23:02:20 +0900

    アーキテクチャに基づくディレクトリ構造を作成

---
+++ WantedlyIosApp/WantedlyIosApp/screens/recruitments/RecruitmentsViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftUI
+
+@MainActor
+class RecruitmentsViewModel: ObservableObject {
+    
+    init() {
+        // 初期化処理
+    }
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+} 
