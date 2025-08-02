87f474d12bee8146714a5bd4936a896094d0e93c
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-08-02 21:12:53 +0900

    feat: RecruitmentsViewModelにUiStateとIntentパターンを実装

--- WantedlyIosApp/WantedlyIosApp/screens/recruitments/RecruitmentsViewModel.swift
+++ WantedlyIosApp/WantedlyIosApp/screens/recruitments/RecruitmentsViewModel.swift
@@ -1,8 +1,14 @@
 import Foundation
 import SwiftUI
 
+struct RecruitmentsUiState {
+    var searchText = ""
+    var isLoading = false
+}
+
 @MainActor
 class RecruitmentsViewModel: ObservableObject {
+    @Published private(set) var uiState = RecruitmentsUiState()
     
     init() {
         // 初期化処理
@@ -10,5 +16,22 @@ class RecruitmentsViewModel: ObservableObject {
     
     // MARK: - Public Methods
     
+    func onAction(_ intent: RecruitmentsIntent) {
+        switch intent {
+        case .onAppear:
+            onAppear()
+        case .onSearchTextChanged(let text):
+            onSearchTextChanged(text)
+        }
+    }
+    
     // MARK: - Private Methods
+    
+    private func onAppear() {
+        // 画面表示時の処理
+    }
+    
+    private func onSearchTextChanged(_ text: String) {
+        uiState.searchText = text
+    }
 }
